6bee38a760048e0d2b572d020c3900b33db315d2
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-16 19:18:22 -0300

    Feature: Configurado React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <div>Hello World</div>
     </ThemeProvider>
   )
 }
---
+++ src/Router/index.tsx
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { Blog } from '../pages/Blog'
+import { Post } from '../pages/Post'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Blog />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Route>
+    </Routes>
+  )
+}
